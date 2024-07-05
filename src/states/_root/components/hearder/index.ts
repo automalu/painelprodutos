32762c03b9f3ba6d4117668cba3441098141a77a
Evandro Uzeda <evandrouzeda@gmail.com>
2024-07-05 15:17:09 +0000

    header pronto

--- src/states/_root/components/hearder/index.ts
+++ src/states/_root/components/hearder/index.ts
@@ -2,17 +2,26 @@ import Z from "zeyo";
 import App from "../../../../app";
 import LayoutContainerBreakout from "../../../../componentes/layoutContainer/breakout";
 import style from "./style.module.css"
+const flex = {
+    jcBetween: ["d-flex", "jc-between"]
+}
 export default class Header extends LayoutContainerBreakout<"header"> {
     constructor(app: App) {
         super("header")
         
-        this.children(
+        this.class(...flex.jcBetween).children(
             Z("div").class(style.logo).children(
                 Z("img").set("src", "/_img/logo.png"),
                 Z("span").children(
                     Z("h4").text("Automalu"),
                 )
             ),
+            Z("nav").class(...flex.jcBetween, style.nav).children(
+                Z("h4").text("Home"),
+                Z("h4").text("Sobre nós"),
+                Z("h4").text("Dicas"),
+                Z("button").text("Entrar")
+            )
         )
     }
 }
